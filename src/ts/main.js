5343a8af27e65546c9debda26e7d05baf3e07a4d
Eeemlo <emma.lorensson@outlook.com>
2024-03-25 10:32:31 +0100

    Funktionalitet tillagd

--- src/ts/main.js
+++ src/ts/main.js
@@ -1,20 +1,116 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
-};
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-};
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+//Ladda in DOM innan JS körs
+document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.querySelector("#myModal"); //Container för popup
+    var btn = document.querySelector("#myBtn"); //Knapp för att öppna popup
+    var closeBtn = document.querySelector(".close"); //Kryss för att stänga popup
+    var form = document.querySelector(".courseform");
+    var courseListContainer = document.querySelector(".courselist-container");
+    var coursecodeInput = document.getElementById("coursecode");
+    var coursenameInput = document.getElementById("coursename");
+    var progressionInput = document.getElementById("progression");
+    var syllabusInput = document.getElementById("syllabus");
+    var deleteBtn = document.querySelector("#deleteBtn"); //Knapp för att öppna popup
+    //Ladda in sparade kurser vid sidladdning
+    displayCourses();
+    // Kontrollera att modal, btn och span har validerats innan de används
+    if (modal && btn && closeBtn) {
+        // When the user clicks on <button>, open the modal
+        btn.onclick = function () {
+            modal.style.display = "block";
+        };
+        // When the user clicks on <span> (x), close the modal
+        closeBtn.onclick = function () {
+            modal.style.display = "none";
+        };
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        };
     }
-};
+    ;
+    ;
+    //Funktion som lagrar kurs i localStorage
+    function addCourse(course) {
+        localStorage.setItem(course.coursecode, JSON.stringify(course));
+        displayCourses();
+    }
+    ;
+    //Funktion för att ladda sparade kurser från localStorage
+    function loadCourses() {
+        for (var i = 0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            var course = JSON.parse(localStorage.getItem(key));
+            displayCourse(course);
+        }
+    }
+    ;
+    //Funktion som skriver ut enskild kurs till DOM
+    function displayCourse(course) {
+        var courseElement = document.createElement("div");
+        courseElement.innerHTML = "\n    <div class=\"course\">\n      <h3>".concat(course.coursename, "</h3>\n      <p><strong>Kurskod:</strong> ").concat(course.coursecode, "</p>\n                <p><strong>Progression:</strong> ").concat(course.progression, "</p>\n                <p><strong>L\u00E4nk till kursplan:</strong> <a href=\"").concat(course.syllabus, "\">").concat(course.syllabus, "</a></p>\n                <a href=\"#\" class=\"edit\" data-coursecode=\"").concat(course.coursecode, "\">\u00C4ndra</a>\n                <a href=\"#\" class=\"delete\" data-coursecode=\"").concat(course.coursecode, "\">Ta bort</a>\n                </div>");
+        courseListContainer.appendChild(courseElement);
+        editCourse();
+        deleteCourse();
+    }
+    //Funktion för att visa samtiga sparade kurser
+    function displayCourses() {
+        courseListContainer.innerHTML = "";
+        loadCourses();
+    }
+    //Funktion för att ändra enskild post i formuläret
+    function editCourse() {
+        var editLinks = document.querySelectorAll(".edit");
+        editLinks.forEach(function (editLink) {
+            editLink.addEventListener("click", function (event) {
+                event.preventDefault();
+                var editLink = event.currentTarget;
+                var courseCode = editLink.dataset.coursecode;
+                var course = JSON.parse(localStorage.getItem(courseCode));
+                //Fyll i formulärfälten med datan för kursen
+                coursecodeInput.value = course.coursecode;
+                coursenameInput.value = course.coursename;
+                progressionInput.value = course.progression;
+                syllabusInput.value = course.syllabus;
+                //Visa popupen igen
+                modal.style.display = "block";
+            });
+        });
+    }
+    ;
+    //Funktion för att radera enskild kurs
+    function deleteCourse() {
+        var deleteLinks = document.querySelectorAll(".delete");
+        deleteLinks.forEach(function (deleteLink) {
+            deleteLink.addEventListener("click", function (event) {
+                var _a;
+                event.preventDefault();
+                var deleteLink = event.currentTarget;
+                var courseCode = deleteLink.dataset.coursecode;
+                localStorage.removeItem(courseCode);
+                (_a = deleteLink.parentElement) === null || _a === void 0 ? void 0 : _a.remove();
+            });
+        });
+    }
+    ;
+    //Eventlyssnare som lägger till ny kurs vid klick på submit-knapp
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        var course = {
+            coursecode: coursecodeInput.value,
+            coursename: coursenameInput.value,
+            progression: progressionInput.value,
+            syllabus: syllabusInput.value
+        };
+        addCourse(course); //Lägg till kurs till LocalStorage och uppdatera listan
+        modal.style.display = "none"; // Dölj popup
+        form.reset(); //Återställ formulär
+        console.log(localStorage);
+    });
+    //Eventlyssnare som raderar kurslistan vid klick på knapp
+    deleteBtn.addEventListener("click", function (event) {
+        localStorage.clear();
+        courseListContainer.innerHTML = "";
+    });
+});
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,25 +1,147 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+//Ladda in DOM innan JS körs
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector("#myModal") as HTMLDivElement; //Container för popup
+  const btn = document.querySelector("#myBtn") as HTMLButtonElement; //Knapp för att öppna popup
+  const closeBtn = document.querySelector(".close") as HTMLSpanElement; //Kryss för att stänga popup
+  const form = document.querySelector(".courseform") as HTMLFormElement;
+  const courseListContainer = document.querySelector(".courselist-container") as HTMLDivElement;
+  const coursecodeInput = document.getElementById("coursecode") as HTMLInputElement;
+  const coursenameInput = document.getElementById("coursename") as HTMLInputElement
+  const progressionInput = document.getElementById("progression") as HTMLInputElement
+  const syllabusInput = document.getElementById("syllabus") as HTMLInputElement
+  const deleteBtn = document.querySelector("#deleteBtn") as HTMLButtonElement; //Knapp för att öppna popup
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+  //Ladda in sparade kurser vid sidladdning
+  displayCourses();
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  // Kontrollera att modal, btn och span har validerats innan de används
+  if (modal && btn && closeBtn) {
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+    // When the user clicks on <button>, open the modal
+    btn.onclick = () => {
+      modal.style.display = "block";
+    };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    // When the user clicks on <span> (x), close the modal
+    closeBtn.onclick = () => {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    };
+  };
+
+  //Skapa interface Course
+  interface Course {
+    coursecode: string;
+    coursename: string;
+    progression: string;
+    syllabus: string;
+  };
+
+
+  //Funktion som lagrar kurs i localStorage
+  function addCourse(course: Course): void {
+    localStorage.setItem(course.coursecode, JSON.stringify(course));
+    displayCourses();
+  };
+
+  //Funktion för att ladda sparade kurser från localStorage
+  function loadCourses(): void {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i)!;
+      const course = JSON.parse(localStorage.getItem(key)!) as Course;
+      displayCourse(course);
+    }
+  };
+
+  //Funktion som skriver ut enskild kurs till DOM
+  function displayCourse(course: Course): void {
+    const courseElement = document.createElement("div");
+    courseElement.innerHTML = `
+    <div class="course">
+      <h3>${course.coursename}</h3>
+      <p><strong>Kurskod:</strong> ${course.coursecode}</p>
+                <p><strong>Progression:</strong> ${course.progression}</p>
+                <p><strong>Länk till kursplan:</strong> <a href="${course.syllabus}">${course.syllabus}</a></p>
+                <a href="#" class="edit" data-coursecode="${course.coursecode}">Ändra</a>
+                <a href="#" class="delete" data-coursecode="${course.coursecode}">Ta bort</a>
+                </div>`;
+    courseListContainer.appendChild(courseElement);
+    editCourse();
+    deleteCourse();
   }
-}
+
+  //Funktion för att visa samtiga sparade kurser
+  function displayCourses(): void {
+    courseListContainer.innerHTML = "";
+    loadCourses();
+  }
+
+  //Funktion för att ändra enskild post i formuläret
+function editCourse(): void {
+  const editLinks = document.querySelectorAll(".edit");
+  editLinks.forEach((editLink) => {
+    editLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      const editLink = event.currentTarget as HTMLElement;
+      const courseCode = editLink.dataset.coursecode!;
+      const course = JSON.parse(localStorage.getItem(courseCode)!);
+      //Fyll i formulärfälten med datan för kursen
+      coursecodeInput.value = course.coursecode;
+      coursenameInput.value = course.coursename;
+      progressionInput.value = course.progression;
+      syllabusInput.value = course.syllabus;
+      //Visa popupen igen
+      modal.style.display ="block";
+    });
+  });
+};
+
+//Funktion för att radera enskild kurs
+function deleteCourse(): void {
+  const deleteLinks = document.querySelectorAll(".delete");
+  deleteLinks.forEach((deleteLink) => {
+    deleteLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      const deleteLink = event.currentTarget as HTMLElement;
+      const courseCode = deleteLink.dataset.coursecode!;
+      localStorage.removeItem(courseCode);
+      deleteLink.parentElement?.remove();
+    });
+  });
+};
+
+  //Eventlyssnare som lägger till ny kurs vid klick på submit-knapp
+  form.addEventListener("submit", (event: Event) => {
+    event.preventDefault();
+
+    const course: Course = {
+      coursecode: coursecodeInput.value,
+      coursename: coursenameInput.value,
+      progression: progressionInput.value,
+      syllabus: syllabusInput.value
+    };
+
+    addCourse(course); //Lägg till kurs till LocalStorage och uppdatera listan
+    modal.style.display = "none"; // Dölj popup
+    form.reset(); //Återställ formulär
+    console.log(localStorage)
+  });
+
+  //Eventlyssnare som raderar kurslistan vid klick på knapp
+  deleteBtn.addEventListener("click", (event: Event) => {
+
+    localStorage.clear();
+    courseListContainer.innerHTML = "";
+  });
+
+
+});
+
+
